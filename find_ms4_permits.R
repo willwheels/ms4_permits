8d0faf74b524ae5507a137d45cfeb226dde754b8
willwheels <wheeler.william@epa.gov>
2025-09-29 10:05:04 -0400

    initial scripts

---
+++ find_ms4_permits.R
@@ -0,0 +1,52 @@
+library(tidytable)
+
+if(!dir.exists(here::here("data"))) {dir.create(here::here("data"))}
+
+
+options(timeout = 600)
+if(!file.exists(here::here("data", "npdes_downloads.zip"))) { 
+  download.file("https://echo.epa.gov/files/echodownloads/npdes_downloads.zip",
+                destfile = here::here("data", "npdes_downloads.zip"))
+  
+  unzip(here::here("data", "npdes_downloads.zip"),
+        files = c("ICIS_PERMITS.csv", "NPDES_PERM_COMPONENTS.csv"),
+        exdir = here::here("data"))
+}
+
+npdes_perm_components <- fread(here::here("data", "NPDES_PERM_COMPONENTS.csv"))
+
+npdes_perm_components |>
+  group_by(COMPONENT_TYPE_CODE, COMPONENT_TYPE_DESC) |>
+  count(sort = TRUE)
+
+npdes_perm_components <- npdes_perm_components |>
+  filter(COMPONENT_TYPE_CODE %in% c("SWS", "SWM"))
+
+icis_permits <- fread(here::here("data", "ICIS_PERMITS.csv"))
+
+icis_permits <- icis_permits |>
+  left_join(npdes_perm_components) |>
+  filter(!is.na(COMPONENT_TYPE_CODE))
+
+icis_permits |>
+  group_by(EXTERNAL_PERMIT_NMBR) |>
+  summarise(num_versions = n()) |>
+  ungroup() |>
+  summarise(mean_versions = mean(num_versions))
+
+icis_permits_large <- icis_permits |>
+  filter(COMPONENT_TYPE_CODE == "SWM") |>
+  select(EXTERNAL_PERMIT_NMBR, ORIGINAL_ISSUE_DATE) |>
+  distinct() |>
+  mutate(original_date = lubridate::mdy(ORIGINAL_ISSUE_DATE)) |>
+  arrange(original_date)
+
+
+icis_permits_small <- icis_permits |>
+  filter(COMPONENT_TYPE_CODE == "SWS") |>
+  select(EXTERNAL_PERMIT_NMBR, ORIGINAL_ISSUE_DATE) |>
+  distinct() |>
+  mutate(original_date = lubridate::mdy(ORIGINAL_ISSUE_DATE)) |>
+  arrange(original_date)
+
+
